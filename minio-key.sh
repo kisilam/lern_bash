075eba4a48605ed53e4c27750b0d618b30a69d28
kisilam <kisilam@ukr.net>
2022-10-17 03:07:04 +0300

    script for min.io service SSL

---
+++ minio-key.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#dst_dir for key
+DS_SSL_DIR=/home/sammy/.minio/certs/docs.movingboardsusa.com
+#Domain
+LE_DOMAIN=docs.movingboardsusa.com
+#User
+KEY_USER=minio-user
+#Group
+KEY_GROUP=minio-user
+#Service
+
+cp -H /etc/letsencrypt/live/$LE_DOMAIN/privkey.pem $DS_SSL_DIR/private.key
+cp -H /etc/letsencrypt/live/$LE_DOMAIN/fullchain.pem $DS_SSL_DIR/public.crt
+
+chown $KEY_USER:$KEY_GROUP $DS_SSL_DIR/*
+#chmod 660 $EXIM_SSL_DIR/*
+
+/bin/systemctl restart minio.service
