2cb71499d9d8aa5fea08ada50585206c787ce123
kisilam <kisilam@ukr.net>
2023-05-05 12:46:58 +0300

    Update exim-key.sh

--- exim/exim-key.sh
+++ exim/exim-key.sh
@@ -2,7 +2,7 @@
 #dst_dir for exim key
 EXIM_SSL_DIR=/etc/exim4/ssl
 #Domain
-LE_DOMAIN=mail.zozulya.sale
+LE_DOMAIN=mail.example.com
 
 cp -H /etc/letsencrypt/live/$LE_DOMAIN/privkey.pem $EXIM_SSL_DIR/privkey.pem
 cp -H /etc/letsencrypt/live/$LE_DOMAIN/fullchain.pem $EXIM_SSL_DIR/fullchain.pem
